dab06660ba4e4e60f8acea3cff54efc345843c31
Nisha Jain <nj241098@gmail.com>
2022-02-10 18:34:41 +0530

    Initial commit

---
+++ app/src/main/java/com/nishajain/linechartjetpack/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.nishajain.linechartjetpack
+
+import android.graphics.PointF
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.nishajain.linechartjetpack.ui.theme.LineChartJetPackTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LineChartJetPackTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = {
+                                    Text(
+                                        text = "Line Chart",
+                                        modifier = Modifier.fillMaxWidth(),
+                                        textAlign = TextAlign.Center
+                                    )
+                                }
+                            )
+                        }
+                    ) {
+                        LineChart()
+                    }
+                }
+            }
+        }
+    }
+}
+
+//Static value
+private fun lineChartData() = listOf(
+    5929, 6898, 8961, 5674, 7122, 2092, 3427, 5520, 4680, 7418,
+    4743, 4280, 12211, 7295, 9900, 12438, 11186, 5439, 4227, 5138,
+    11015, 8386, 12450, 10411, 10852, 7782, 7371, 4983, 9234, 6847
+)
+
+@Composable
+fun LineChart() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(360.dp)
+            .padding(16.dp),
+        elevation = 10.dp
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .wrapContentSize(align = Alignment.BottomStart)
+        ) {
+            Canvas(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(250.dp)
+            ) {
+                val distance = size.width / (lineChartData().size + 1)
+                var currentX = 0F
+                val maxValue = lineChartData().maxOrNull() ?: 0
+                val points = mutableListOf<PointF>()
+
+                lineChartData().forEachIndexed { index, data ->
+                    if (lineChartData().size >= index + 2) {
+                        val y0 = (maxValue - data) * (size.height / maxValue)
+                        val x0 = currentX + distance
+                        points.add(PointF(x0, y0))
+                        currentX += distance
+                    }
+                }
+
+                for (i in 0 until points.size - 1) {
+                    drawLine(
+                        start = Offset(points[i].x, points[i].y),
+                        end = Offset(points[i + 1].x, points[i + 1].y),
+                        color = Color(0xFF3F51B5),
+                        strokeWidth = 8f
+                    )
+                }
+            }
+        }
+    }
+}
+
+
